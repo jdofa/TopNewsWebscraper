2058f1b39f5f293ed19407fcfe4712cc3604e327
dolfo <joserodolfonieblas@gmail.com>
2022-11-09 23:48:27 -0800

    lists top headlines

--- webscraper.py
+++ webscraper.py
@@ -4,17 +4,11 @@ import requests
 url = 'https://news.google.com/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRFZxYUdjU0FtVnVHZ0pWVXlnQVAB?hl=en-US&gl=US&ceid=US%3Aen'
 news_html = requests.get(url).text
 html_text = BeautifulSoup(news_html, 'lxml')
-headlines = html_text.find_all('h4')
-links = html_text.find_all('a')
+articles = html_text.find_all('article')
+i = 1
+for article in articles: #Lists top headlines, need to remove things that don't belong to headline
+    print(f'{i} : {article.text}')
+    i += 1
 
-#for i in range(10):
-    #print(headlines[i+1].text)
-    #print(links[i+1].get('href'))
-    
-for link in links:
-    st = link.get('href')
-    if st is not None:
-        if(st[0] == '.'):
-            if 'articles' in st:
-                print('https://news.google.com' + st[1:])
+
 
