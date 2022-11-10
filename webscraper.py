3a5288a9dfe11edaebecbfa07f610732ecd25266
dolfo <joserodolfonieblas@gmail.com>
2022-11-10 01:19:03 -0800

    Webscraper Completed
    Grabs top 10 headlines on google news and displays the titles of the articles

--- webscraper.py
+++ webscraper.py
@@ -1,14 +1,20 @@
 from bs4 import BeautifulSoup
 import requests
 
+
 url = 'https://news.google.com/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRFZxYUdjU0FtVnVHZ0pWVXlnQVAB?hl=en-US&gl=US&ceid=US%3Aen'
 news_html = requests.get(url).text
 html_text = BeautifulSoup(news_html, 'lxml')
 articles = html_text.find_all('article')
+
+print("- TOP 10 HEADLINES ON GOOGLE NEWS - ")
 i = 1
-for article in articles: #Lists top headlines, need to remove things that don't belong to headline
-    print(f'{i} : {article.text}')
-    i += 1
-
-
-
+amount = 10
+for article in articles: 
+    if(i <= amount):
+        headline = article.h4
+        if headline is not None:
+            print(f'{i} : {headline.text}')
+            i += 1
+    else:
+        break
