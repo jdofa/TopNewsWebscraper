1e960c6f4c04b312316d1baf025ed89bdf1a2345
dolfo <joserodolfonieblas@gmail.com>
2022-12-13 21:30:25 -0800

    Minor Improvements
    - Program stops so user can read
    - Rid of pointless variable

--- webscraper.py
+++ webscraper.py
@@ -1,20 +1,23 @@
 from bs4 import BeautifulSoup
 import requests
 
-
 url = 'https://news.google.com/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRFZxYUdjU0FtVnVHZ0pWVXlnQVAB?hl=en-US&gl=US&ceid=US%3Aen'
 news_html = requests.get(url).text
 html_text = BeautifulSoup(news_html, 'lxml')
 articles = html_text.find_all('article')
 
 print("- TOP 10 HEADLINES ON GOOGLE NEWS - ")
+
 i = 1
-amount = 10
 for article in articles: 
-    if(i <= amount):
+    if(i <= 10):
         headline = article.h4
         if headline is not None:
             print(f'{i} : {headline.text}')
             i += 1
     else:
         break
+
+input("\n - Enter any key to end the program: ")
+
+
